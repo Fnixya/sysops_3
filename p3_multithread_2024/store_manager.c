77033f8cdadf842e978f0002f43c5fc6fd36146c
Fnixya <62068603+Fnixya@users.noreply.github.com>
2024-04-22 16:13:55 +0200

    copia de my_strtol

--- p3_multithread_2024/store_manager.c
+++ p3_multithread_2024/store_manager.c
@@ -12,38 +12,86 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
+/* Constants _______________________________________________________________________________________________________ */
 #define MAX_THREADS 16
 
-// Functions
+
+/* Functions _______________________________________________________________________________________________________ */
 int read_file(char *file_name);
+int my_strtol(char *string, long *number);
 int producer();
 int consumer();
 
 /***
  * Read the file
+ * @param file_name: file name
+ * @return -1 if error, 0 if success
 */
 int read_file(char *file_name) {
   return -1;
 }
 
 /***
+ * Conversion from string to long integer using strtol with some error handling
+ * @param string: string to convert to long
+ * @param number: pointer to store the result
+ * @return Error -1 otherwise 0 */
+int my_strtol(char *string, long *number) {
+    // https://stackoverflow.com/questions/8871711/atoi-how-to-identify-the-difference-between-zero-and-error
+    char *nptr, *endptr = NULL;                            /* pointer to additional chars  */
+    
+    nptr = string;
+    endptr = NULL;
+    errno = 0;
+    *number = strtol(nptr, &endptr, 10);
+
+    // Error extracting number (it is not an integer)
+    if (nptr && *endptr != 0) {
+        if (mode == 0)
+            fprintf(stdout, "[ERROR] Not an integer\n");
+        return -1;
+    }
+    // Overflow
+    else if (errno == ERANGE && *number == LONG_MAX)
+    {
+        if (mode == 0)
+            fprintf(stdout, "[ERROR] Overflow\n", var);
+        return -1;
+    }
+    // Underflow
+    else if (errno == ERANGE && *number == LONG_MIN)
+    {
+        if (mode == 0)
+            fprintf(stdout, "[ERROR] Underflow\n", var);
+        return -1;
+    }
+
+    return 0;
+}
+
+/***
  * Producer function
+ * @return -1 if error, 0 if success
 */
 int producer() {
   pthread_exit(0);
+  return 0;
 }
 
 /***
  * Consumer function
+ * @return -1 if error, 0 if success
 */
 int consumer() {
   pthread_exit(0);
+  return 0;
 }
 
 
 
 /***
- * Main function
+ * Main function _____________________________________________________________________________________________________
 */
 int main (int argc, const char * argv[])
 {
@@ -54,11 +102,11 @@ int main (int argc, const char * argv[])
   }
 
   char *file_name = argv[1];
+  long num_producers, num_consumers, buffer_size;
 
-  // Change to strtol in the future
-  int num_producers = atoi(argv[2]),
-    num_consumers = atoi(argv[3]),
-    buffer = atoi(argv[4]);
+  if (my_strtol(argv[2], &num_producers) == -1) return -1;
+  if (my_strtol(argv[3], &num_consumers) == -1) return -1;
+  if (my_strtol(argv[4], &buffer_size) == -1) return -1;
 
   if (N < 1) {
     printf("ERROR: The number of consumers must be greater than 0\n");
